cec7810a78e274ac658c93acab098184e2580fae
kaushik puntambekar <k08puntambekar@gmail.com>
2021-02-18 18:18:35 +0530

    An example to use GitHub

---
+++ src/githubexample/GitHubExample.java
@@ -0,0 +1,7 @@
+package githubexample;
+
+public class GitHubExample {
+    public static void main(String[] args) {
+        System.out.println("This is a example to push and pull the github code.");
+    }
+}
